8741d80c9beb0e38f3e7407a448f2f7b02fc6f88
Steve Witman <stevewitman@gmail.com>
2016-09-17 23:59:16 -0600

    object graph

--- app.js
+++ app.js
@@ -2,37 +2,16 @@ var cfg = require("./knex-cfg").pg;
 var knex = require("knex")(cfg);
 var screen = require("./screen");
 var Treeize = require("treeize");
+var Promise = require("bluebird");
 
 screen.clear();
+var pAuthorRows = knex("author").where("id", 1).debug(false).then();
+var pBooks = knex("book").where("author_id", 1).debug(false).then();
 
-var query = knex("book")
-      .join("author", "author_id", "=", "book.author_id")
-      .select("author.firstname", "author.lastname", "book.title as books:title", "book.rating as books:rating", "book.id as books:id")
-      .where("author.id", 1).debug(false)
-      .then(function(rows) {
-        var tree = new Treeize();
-        tree.grow(rows);
-        var authors = tree.getData();
-        screen.write(authors[0], "json");
-      })
-      .finally(function() {
-        knex.destroy();
-      })
-
-
-
-
-run(query, "pretty");
-
-function run(knexQuery, mode) {
-  return knexQuery.then(function(rows) {
-    screen.write(rows, mode);
-  })
-  .catch(function(err) {
-    screen.write("Oops");
-    screen.write(err);
-  })
-  .finally(function() {
-    knex.destroy();
-  })
-}
+Promise.all([pAuthorRows, pBooks]).then(function(results) {
+  var author = results[0][0];
+  author.books = results[1];
+  screen.write(author, "json");
+}).finally(function() {
+  knex.destroy();
+})
