86accf2f29e9e881eb0eaed825bd93936238af3f
Steve Witman <stevewitman@gmail.com>
2016-09-17 23:29:15 -0600

    more queries

--- app.js
+++ app.js
@@ -4,15 +4,45 @@ var screen = require("./screen");
 
 screen.clear();
 
-// var query = knex.select("title", "rating").from("book");
-// var query = knex.select("title", "rating").from("book").first();
-// var query = knex.select("title", "rating").from("book").limit(2);
-// var query = knex.select("rating").from("book");
-// var query = knex.select("rating").from("book").distinct();
-// var query = knex.select(knex.raw("COUNT(*) as BookCount")).from("book")
-// var query = knex.raw("SELECT * FROM book where author_id = 1")
-var author_id = 1;
-var query = knex.raw("SELECT * FROM book where author_id = ?", [author_id]);
+// var query = knex.select("title", "rating").from("book").orderByRaw('title desc');
+
+// var query = knex.select("title", "id`").from("book").orderBy('id').limit(2).offset(2);
+
+// var query = knex("book").min("rating as lowScore");
+
+// var query = knex("book").select("author_id").min("rating as lowScore").groupBy("author_id");
+
+// var query = knex("author").where({"firstname": "Mark", "lastname": "Twain"});
+
+// var query = knex("author").where("id", 1);
+
+// var query = knex("author").where("id", "<>", 1);
+
+// var query = knex("author").where("id", "in", [1,2,3]);
+
+// var subquery = knex("author").select("id").where("id", ">", 1);
+// var query = knex("author").where("id", "in", subquery);
+
+// var query = knex("author").where(function() {
+//   this.where("id", 1).orWhere("id", ">", 3);
+// });
+
+// var query = knex("author").where(function() {
+//   this.where("id", 1).orWhere("id", ">", 3);
+// }).orWhere({firstname: "Mark"});
+
+// var query = knex("book").whereExists(function() {
+//   this.from("author").whereRaw("1=1")
+// });
+
+// var query = knex("book").join("author", "author_id", "=", "book.author_id").select("author.firstname", "author.lastname", "book.title");
+
+var query = knex("book").join("author", function() {
+  this.on("author_id", "=", "book.author_id")
+}).select("author.firstname", "author.lastname", "book.title");
+
+
+
 
 run(query, "pretty");
 
