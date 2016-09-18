27e9d88d2f42b4bc84e748cdb0efaf686c156393
Steve Witman <stevewitman@gmail.com>
2016-09-17 23:51:01 -0600

    used treeize to return an object from query

--- app.js
+++ app.js
@@ -1,45 +1,23 @@
 var cfg = require("./knex-cfg").pg;
 var knex = require("knex")(cfg);
 var screen = require("./screen");
+var Treeize = require("treeize");
 
 screen.clear();
 
-// var query = knex.select("title", "rating").from("book").orderByRaw('title desc');
-
-// var query = knex.select("title", "id`").from("book").orderBy('id').limit(2).offset(2);
-
-// var query = knex("book").min("rating as lowScore");
-
-// var query = knex("book").select("author_id").min("rating as lowScore").groupBy("author_id");
-
-// var query = knex("author").where({"firstname": "Mark", "lastname": "Twain"});
-
-// var query = knex("author").where("id", 1);
-
-// var query = knex("author").where("id", "<>", 1);
-
-// var query = knex("author").where("id", "in", [1,2,3]);
-
-// var subquery = knex("author").select("id").where("id", ">", 1);
-// var query = knex("author").where("id", "in", subquery);
-
-// var query = knex("author").where(function() {
-//   this.where("id", 1).orWhere("id", ">", 3);
-// });
-
-// var query = knex("author").where(function() {
-//   this.where("id", 1).orWhere("id", ">", 3);
-// }).orWhere({firstname: "Mark"});
-
-// var query = knex("book").whereExists(function() {
-//   this.from("author").whereRaw("1=1")
-// });
-
-// var query = knex("book").join("author", "author_id", "=", "book.author_id").select("author.firstname", "author.lastname", "book.title");
-
-var query = knex("book").join("author", function() {
-  this.on("author_id", "=", "book.author_id")
-}).select("author.firstname", "author.lastname", "book.title");
+var query = knex("book")
+      .join("author", "author_id", "=", "book.author_id")
+      .select("author.firstname", "author.lastname", "book.title as books:title", "book.rating as books:rating", "book.id as books:id")
+      .where("author.id", 1).debug(false)
+      .then(function(rows) {
+        var tree = new Treeize();
+        tree.grow(rows);
+        var authors = tree.getData();
+        screen.write(authors[0], "json");
+      })
+      .finally(function() {
+        knex.destroy();
+      })
 
 
 
